862cd56b0cc8e83e2463d1c86df32f3be9ee9aa3
Linead96 <d.garbosa96@gmail.com>
2018-12-23 17:19:11 +0000

    add mongo lab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes        = require("./routes/comments"),
     campgroundRoutes     = require("./routes/campground"),
     indexRoutes          = require("./routes/index");
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v11", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp_v11", {useNewUrlParser: true});
+mongoose.connect("mongodb://daniel:daniel96@ds243084.mlab.com:43084/yelpcamp1996", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
