ab48de57b156a2596a5c4bd7bbc3e1c645b9f174
Linead96 <d.garbosa96@gmail.com>
2018-12-23 18:09:06 +0000

    update dburl

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ var express     = require("express"),
 var commentRoutes        = require("./routes/comments"), 
     campgroundRoutes     = require("./routes/campground"),
     indexRoutes          = require("./routes/index");
-    
-//mongoose.connect("mongodb://localhost:27017/yelp_camp_v11", {useNewUrlParser: true});
-mongoose.connect("mongodb://daniel:daniel96@ds243084.mlab.com:43084/yelpcamp1996", {useNewUrlParser: true});
+  
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+//mongoose.connect("mongodb://daniel:daniel96@ds243084.mlab.com:43084/yelpcamp1996", {useNewUrlParser: true});
+
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
